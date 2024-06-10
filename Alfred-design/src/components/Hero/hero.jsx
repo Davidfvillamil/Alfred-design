9c21f713443dc0940327c4f75d13e908cf2dd121
David Villamil <davidfvillamilm@gmail.com>
2024-06-09 21:43:43 -0500

    agregar imagen al Hero

--- Alfred-design/src/components/Hero/hero.jsx
+++ Alfred-design/src/components/Hero/hero.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
+import './hero.css'
 
 function Hero() {
   return (
     <>
-        <section id='#hero' className='min-vh-100 d-flex align-items-center text-center'>
+        <section id='hero' className='min-vh-100 d-flex align-items-center text-center'>
             <div className="container">
                 <div className="row">
                     <div className="col-12">
-                        <h1 className='text-uppercase fw-semibold display-1'>Alfred will do it!</h1>
-                        <h5>Monthly based Design Suscription</h5>
+                        <h1 className='title-h1 text-uppercase fw-semibold display-1'>Alfred will do it!</h1>
+                        <h5 className='paragraph'>Monthly based Design Suscription</h5>
                         <div>
                             <a href="" className='btn btn-brand'>Get Started</a>
                             <a href="" className='btn btn-light'>Check portafolio</a>
