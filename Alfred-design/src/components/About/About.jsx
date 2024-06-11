5821171ad9c7a91132f538c0b332ddd21ec441fb
David Villamil <davidfvillamilm@gmail.com>
2024-06-10 22:16:30 -0500

    terminar la maquetación del About

--- Alfred-design/src/components/About/About.jsx
+++ Alfred-design/src/components/About/About.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
-import About_foto from '../../assets/About2.jpg'
+import About_foto from '../../assets/About4.jpg'
 import './About.css'
+
+import { GrSend } from "react-icons/gr";
+
 
 function About() {
   return (
@@ -15,17 +18,66 @@ function About() {
                     </div>
                 </div>
             </div>
-            <div className="row">
+            <div className="row justify-content-between align-items-center">
                 <div className="col-lg-6">
                     <img src={About_foto} alt="" className="responsive-img"/>
                 </div>
                 <div className="col-lg-5">
                     <h1>About Alfred</h1>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea quibusdam rerum exercitationem obcaecati in suscipit.</p>
-                    <div>
-                        <div className="iconbox"></div>
+                    <p className='mt-3 mb-4'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea quibusdam rerum exercitationem obcaecati in suscipit.</p>
+                    <div className='d-flex pt-4 mb-3'>
+                        <div className="iconbox me-3">
+                            <GrSend />
+                        </div>
+                        <div>
+                            <h5>We are awesome</h5>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo perspiciatis facilis tempore voluptatibus.</p>
+                        </div>
+                    </div>
+
+                    <div className='d-flex mb-3'>
+                        <div className="iconbox me-3">
+                            <GrSend />
+                        </div>
+                        <div>
+                            <h5>We are awesome</h5>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo perspiciatis facilis tempore voluptatibus.</p>
+                        </div>
+                    </div>
+
+                    <div className='d-flex'>
+                        <div className="iconbox me-3">
+                            <GrSend />
+                        </div>
+                        <div>
+                            <h5>We are awesome</h5>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo perspiciatis facilis tempore voluptatibus.</p>
+                        </div>
+                    </div>
+
+                    
+                    <div className='d-flex'>
+                        <div className="iconbox me-3">
+                            <GrSend />
+                        </div>
+                        <div>
+                            <h5>We are awesome</h5>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo perspiciatis facilis tempore voluptatibus.</p>
+                        </div>
+                    </div>
+
+                    
+                    <div className='d-flex'>
+                        <div className="iconbox me-3">
+                            <GrSend />
+                        </div>
+                        <div>
+                            <h5>We are awesome</h5>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo perspiciatis facilis tempore voluptatibus.</p>
+                        </div>
                     </div>
                 </div>
+
             </div>
         </div>
     </section>
