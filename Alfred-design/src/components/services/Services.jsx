b05b00d9f60ea3c2015b7f66d7ece87866df8312
David Villamil <davidfvillamilm@gmail.com>
2024-06-20 10:07:10 -0500

    revisado el apartado de servicios

--- Alfred-design/src/components/services/Services.jsx
+++ Alfred-design/src/components/services/Services.jsx
@@ -2,6 +2,13 @@ import React from 'react'
 import './services.css'
 
 import { GoFileMedia } from "react-icons/go";
+import { FaTiktok } from "react-icons/fa6";
+import { IoIosColorPalette } from "react-icons/io";
+import { FaAppStoreIos } from "react-icons/fa";
+import { FaPhotoVideo } from "react-icons/fa";
+import { VscGraph } from "react-icons/vsc";
+import { BiLogoGmail } from "react-icons/bi";
+
 
 function Services() {
   return (
@@ -20,60 +27,60 @@ function Services() {
                 <div className="col-lg-4 col-sm-6">
                     <div className="service theme-shadow p-lg-5 p-4">
                         <div className="iconbox">
-                            <GoFileMedia></GoFileMedia>
+                            <FaTiktok/>
                         </div>
                         <h5 className='mt-4 mb-3'>Social media</h5>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa doloribus vero nobis mollitia.</p>
+                        <p>We deliver engaging social media content designed to captivate your audience on LinkedIn, Instagram, TikTok</p>
                     </div>
                 </div>
 
                 <div className="col-lg-4 col-sm-6">
                     <div className="service theme-shadow p-lg-5 p-4">
                         <div className="iconbox">
-                            <GoFileMedia></GoFileMedia>
+                            <IoIosColorPalette></IoIosColorPalette>
                         </div>
-                        <h5 className='mt-4 mb-3'>Social media</h5>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa doloribus vero nobis mollitia.</p>
+                        <h5 className='mt-4 mb-3'>Unforgettable Brand Design</h5>
+                        <p>Comprehensive brand design services, from logos to brand manuals and full branding packages</p>
                     </div>
                 </div>
 
                 <div className="col-lg-4 col-sm-6">
                     <div className="service theme-shadow p-lg-5 p-4">
                         <div className="iconbox">
-                            <GoFileMedia></GoFileMedia>
+                            <FaAppStoreIos></FaAppStoreIos>
                         </div>
-                        <h5 className='mt-4 mb-3'>Social media</h5>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa doloribus vero nobis mollitia.</p>
+                        <h5 className='mt-4 mb-3'>Ux/Ui</h5>
+                        <p>Whether you need a website, app, or complex interface, we create intuitive designs that elevate user engagement and satisfaction.</p>
                     </div>
                 </div>
 
                 <div className="col-lg-4 col-sm-6">
                     <div className="service theme-shadow p-lg-5 p-4">
                         <div className="iconbox">
-                            <GoFileMedia></GoFileMedia>
+                            <FaPhotoVideo></FaPhotoVideo>
                         </div>
-                        <h5 className='mt-4 mb-3'>Social media</h5>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa doloribus vero nobis mollitia.</p>
+                        <h5 className='mt-4 mb-3'>Video Edition</h5>
+                        <p>we help you levelup your brand with Brand Videos, Tutorials, Demos, MVPs, Podcasts, and More</p>
                     </div>
                 </div>
 
                 <div className="col-lg-4 col-sm-6">
                     <div className="service theme-shadow p-lg-5 p-4">
                         <div className="iconbox">
-                            <GoFileMedia></GoFileMedia>
+                            <VscGraph></VscGraph>
                         </div>
-                        <h5 className='mt-4 mb-3'>Social media</h5>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa doloribus vero nobis mollitia.</p>
+                        <h5 className='mt-4 mb-3'>Visual Design</h5>
+                        <p>Creative designs for decks, commercial presentations, brochures, infographics, and various visual assets</p>
                     </div>
                 </div>
 
                 <div className="col-lg-4 col-sm-6">
                     <div className="service theme-shadow p-lg-5 p-4">
                         <div className="iconbox">
-                            <GoFileMedia></GoFileMedia>
+                            <BiLogoGmail></BiLogoGmail>
                         </div>
-                        <h5 className='mt-4 mb-3'>Social media</h5>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa doloribus vero nobis mollitia.</p>
+                        <h5 className='mt-4 mb-3'>Content Marketing Solutions</h5>
+                        <p>we specialize in creating professional and engaging email templates, newsletters, and blogs.</p>
                     </div>
                 </div>
             </div>
